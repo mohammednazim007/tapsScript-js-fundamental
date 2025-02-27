8610c666de27f4412e9b817c3027ef224bd5d3f8
mohammednazim007 <mohammednazim3629@gmail.com>
2025-02-27 16:46:01 +0600

    feat:(day-three.js):add person object with greet methods and demonstrate Object.freeze behavior

--- src/day-three.js
+++ src/day-three.js
@@ -3,3 +3,17 @@ console.log(typeof undefined); // undefined
 console.log(typeof {}); // object
 console.log(typeof []); // object
 console.log(typeof function () {}); // function
+
+const person = {
+  name: "John",
+  age: 30,
+  greet: function () {
+    console.log("Hello, my name is " + this.name);
+  }, // arrow function
+  greetArrow: () => {
+    console.log("Hello, my name is " + this.name);
+  },
+};
+Object.freeze(person);
+person.name = "Jane";
+console.log(person.name); // John
