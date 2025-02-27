571f4cc56abc293501a7f33aed7b55d325a6211f
mohammednazim007 <mohammednazim3629@gmail.com>
2025-02-27 16:51:29 +0600

    fix:(day-three.js):replace Object.freeze with Object.seal and remove person.name property

--- src/day-three.js
+++ src/day-three.js
@@ -4,17 +4,3 @@ console.log(typeof {}); // object
 console.log(typeof []); // object
 console.log(typeof function () {}); // function
 
-const person = {
-  name: "John",
-  age: 30,
-  greet: function () {
-    console.log("Hello, my name is " + this.name);
-  }, // arrow function
-  greetArrow: () => {
-    console.log("Hello, my name is " + this.name);
-  },
-};
-// Object.freeze(person);
-Object.seal(person);
-delete person.name 
-console.log(person.name); // John
