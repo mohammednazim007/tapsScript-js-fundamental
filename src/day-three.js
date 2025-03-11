570025075e194492204ce8130cee04ff382396d4
mohammednazim007 <mohammednazim3629@gmail.com>
2025-03-11 13:50:14 +0600

    feat:(day-three.js): implement number guessing game with random number generation

--- src/day-three.js
+++ src/day-three.js
@@ -1,7 +1,14 @@
-for (let initial = 0; initial < 5; initial++) {
-  let star = "";
-  for (let i = 0; i < 5; i++) {
-    star += "*";
-  }
-  console.log(star);
-}
+const userGuesNumber = () => {
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+  const userNumber = Number(prompt("Enter your gues number"));
+
+  if (randomNumber === userNumber)
+    return console.log("Oh, both are same number");
+
+  if (randomNumber > userNumber)
+    return console.log(`Computer number is getter the ${userNumber}`);
+
+  if (randomNumber < userNumber)
+    return console.log(`Congratulation you are win number is ${userNumber}`);
+};
+userGuesNumber();
